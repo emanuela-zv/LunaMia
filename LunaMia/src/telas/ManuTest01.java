337b72ca5bdcb80233a9ebf98405ddce907da389
emanu <emanu@LAPTOP-9D0KNVVL>
2026-02-10 20:52:29 -0300

    Branch de Teste 01 Manu

---
+++ LunaMia/src/telas/ManuTest01.java
@@ -0,0 +1,11 @@
+package telas;
+
+public class ManuTest01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Boa noite, este é o primeiro Branch da Manu");
+	}
+
+}
